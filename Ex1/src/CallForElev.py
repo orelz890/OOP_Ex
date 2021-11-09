f1982b76e449979910b64431aa728764507ed675
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2021-11-09 22:50:32 +0200

    new offline_Algo

--- Ex1/src/CallForElev.py
+++ Ex1/src/CallForElev.py
@@ -4,10 +4,11 @@ class callForElev:
 
     def __init__(self,time:float,src:int,dst:int):
         self._arrive = time
+        self._doneTime = -1
         self._src = src
         self._dst = dst
-        self.state = 0;
-        self.allocatedTo = -1;
+        self._state = 1 if dst>src else -1
+        self._allocatedTo = -1
 
     def get_arrive_time(self):
         return self._arrive
@@ -18,5 +19,11 @@ class callForElev:
     def get_dst(self):
         return self._dst
 
+    def get_state(self):
+        return self._state
+
+    def get_done_time(self):
+        return self._doneTime
+
     def set_allocated(self):
-        return self.allocatedTo
+        return self._allocatedTo
