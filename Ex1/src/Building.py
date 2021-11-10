dc1b20af4ace309869d07c80c9051ab9578dc376
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2021-11-10 16:28:32 +0200

    try & except

--- Ex1/src/Building.py
+++ Ex1/src/Building.py
@@ -1,33 +1,37 @@
 import Elevator
 import json
 
+
 class Building:
 
-    def __init__(self,fileBuilding : str):
-        self.Elevators ={}
-        self.init_from_file(self,fileBuilding)
+    def __init__(self, fileBuilding: str):
+        self.Elevators = {}
+        self.init_from_file(self, fileBuilding)
 
     def __str__(self) -> str:
-        ans =f"minFloor: {self._minFloor}, maxFloor: {self._maxFloor}, "
-        for i in range(0, len(self.Elevators),1):
-            ans+= f"Elev{i} "+str(self.Elevators[str(i)])
+        ans = f"minFloor: {self._minFloor}, maxFloor: {self._maxFloor}, "
+        for i in range(0, len(self.Elevators), 1):
+            ans += f"Elev{i} " + str(self.Elevators[str(i)])
         return ans
 
     def __iter__(self):
         return self.Elevators.values().__iter__()
 
-    def init_from_file(self,file_name:str)->None:
-        with open(file_name,"r") as f:
-            data_dict = json.load(f)
-            for k, v in data_dict.items():
-                if k == '_minFloor':
-                    self._minFloor = v
-                    continue
-                if k == '_maxFloor':
-                    self._maxFloor = v
-                    continue
-                for i in v:
-                    elev = Elevator.Elevator(i['_id'], i['_speed'], i['_minFloor'], i['_maxFloor'] ,i['_closeTime'] ,
-                                    i['_openTime'] ,i['_startTime'] , i['_stopTime'])
-                    self.Elevators[str(i['_id'])] = elev
+    def init_from_file(self, file_name: str) -> None:
+        try:
+            with open(file_name, "r") as f:
+                data_dict = json.load(f)
+                for k, v in data_dict.items():
+                    if k == '_minFloor':
+                        self._minFloor = v
+                        continue
+                    if k == '_maxFloor':
+                        self._maxFloor = v
+                        continue
+                    for i in v:
+                        elev = Elevator.Elevator(i['_id'], i['_speed'], i['_minFloor'], i['_maxFloor'], i['_closeTime'],
+                                                 i['_openTime'], i['_startTime'], i['_stopTime'])
+                        self.Elevators[str(i['_id'])] = elev
 
+        except IOError as e:
+            print(e)
--- Ex1/src/InputCalls.py
+++ Ex1/src/InputCalls.py
@@ -1,16 +1,20 @@
 from CallForElev import callForElev
 import csv
 
+
 class InputCalls:
 
-    def __init__(self,fileCalls:str):
-        self.calls =self.init_from_file(fileCalls)
+    def __init__(self, fileCalls: str):
+        self.calls = self.init_from_file(fileCalls)
 
-    def init_from_file(self,fileName:str):
-        with open(fileName,'r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            headers = csv_reader.fieldnames
-            c=[]
-            for row in csv_reader:
-                c.append(callForElev.__init__(row[headers[1]],row[headers[2]],row[headers[3]]))
-            return c
+    def init_from_file(self, fileName: str):
+        try:
+            with open(fileName, 'r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                headers = csv_reader.fieldnames
+                c = []
+                for row in csv_reader:
+                    c.append(callForElev.__init__(row[headers[1]], row[headers[2]], row[headers[3]]))
+                return c
+        except IOError as e:
+            print(e)
