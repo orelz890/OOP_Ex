6958183e1e71b401a03b906cd4d2fb09f604e3dd
Orel Zamler <orelzx13@gmail.com>
2021-11-18 18:18:27 +0200

    build a structure format to Offline + syntax changes

--- Ex1/src/new_dir.py
+++ Ex1/src/new_dir.py
@@ -1,15 +1,17 @@
 import csv
 import sys
-
 from Ex1.src.Building import Building
 from Ex1.src.InputCalls import InputCalls
 
 DOWN = -1
-REST = 0
+LEVEL = 0
 UP = 1
 
-location, status, toSrc, fromSrc, num_of_stops = [], [], [], [], []
-
+cur_location = []
+status = []
+to_src_times = []
+from_src_times = []
+each_elev_stops = []
 
 class Offline:
 
@@ -19,71 +21,67 @@ class Offline:
         self.elevators = self.b.elevators
         self.min_floor = self.b.min_floor
         self.max_floor = self.b.max_floor
-        for i in range(len(self.elevators)):
-            location.append(0)
-            status.append(REST)
-            num_of_stops.append([])
+        for elev in range(len(self.elevators)):
+            cur_location.append(0)
+            status.append(LEVEL)
+            each_elev_stops.append([])
         allocate(self)
-        with open(out_name, 'w', newline="") as f:
-            write = csv.writer(f)
-            write.writerows(self.calls)
-
+        try:
+            with open(out_name, 'w', newline="") as f:
+                write = csv.writer(f)
+                write.writerows(self.calls)
+        except IOError as e:
+            print(e)
 
 def allocate(self):
-    global allocated_elev
-    for call_i in range(len(self.calls)):
-        src = int(float(self.calls[call_i][2]))
-        dest = int(float(self.calls[call_i][3]))
-        if src > self.max_floor or src < self.min_floor or dest > self.max_floor or dest < self.min_floor:
-            raise Exception("Out of the range!")
+    for call_num in range(len(self.calls)):
+        src = int(self.calls[call_num][2])
+        dest = int(self.calls[call_num][3])
         min_time = sys.maxsize
-        for cur_elev in range(len(self.elevators)):
-            if location[cur_elev] == src:
+        number_of_elev = len(self.elevators)
+        for cur_elev in range(number_of_elev):
+            if cur_location[cur_elev] == src:
                 allocated_elev = cur_elev
                 break
-            toSrc.insert(cur_elev, calculateToSrc(self, cur_elev, src))
-            fromSrc.insert(cur_elev, calculateFromSrc(self, dest, cur_elev, src))
-            temp_time = toSrc[cur_elev] + fromSrc[cur_elev]
+            to_src_times.insert(cur_elev, calculateToSrc(self, cur_elev, src))
+            from_src_times.insert(cur_elev, calculateFromSrc(self, dest, cur_elev, src))
+            temp_time = to_src_times[cur_elev] + from_src_times[cur_elev]
             if temp_time < min_time:
                 min_time = temp_time
                 allocated_elev = cur_elev
-        addstops(src, dest, allocated_elev)
-        self.calls[call_i][5] = allocated_elev
+        PickUp(src, dest, allocated_elev)
+        self.calls[call_num][5] = allocated_elev
 
 
-def calculateToSrc(self, cur_elev, src):
-    pass_num = PassNumSrc(cur_elev, src)
-    dif = abs(location[cur_elev] - src)
-    closeTime = self.elevators[cur_elev].get("_closeTime") * pass_num
-    startTime = self.elevators[cur_elev].get("_startTime") * (pass_num + 1)
-    stopTime = self.elevators[cur_elev].get("_stopTime") * (pass_num + 1)
-    openTime = self.elevators[cur_elev].get("_openTime") * (pass_num + 1)
+def calculateToSrc(self, cur_elev: int, src: int):
+    stops_num = StopsOnWayToSrc(cur_elev, src)
+    dif = abs(cur_location[cur_elev] - src)
     speed = self.elevators[cur_elev].get("_speed")
-    toSrc = closeTime + startTime + dif / speed + stopTime + openTime
-    return toSrc
+    closeTime = self.elevators[cur_elev].get("_closeTime") * stops_num
+    startTime = self.elevators[cur_elev].get("_startTime")
+    stopTime = self.elevators[cur_elev].get("_stopTime")
+    openTime = self.elevators[cur_elev].get("_openTime")
+    TimeToSrc = (dif / speed) + closeTime + (startTime + stopTime + openTime) * (stops_num + 1)
+    return TimeToSrc
 
+def calculateFromSrc(self, dest: int, cur_elev: int, src: int):
+    pass
 
-def PassNumSrc(cur_elev, src):
-    pass_num = 0
+def StopsOnWayFromSrc(dest: int, cur_elev: int):
+    pass
+
+def StopsOnWayToSrc(cur_elev: int, src: int):
+    stops_num = 0
     if status[cur_elev] != DOWN:
-        for i in range(len(num_of_stops[cur_elev])):
-            if src > num_of_stops[cur_elev][i]:
-                pass_num = pass_num + 1
+        for i in range(len(each_elev_stops[cur_elev])):
+            if src > each_elev_stops[cur_elev][i]:
+                stops_num += 1
     else:
-        for i in range(len(num_of_stops[cur_elev])):
-            if src < num_of_stops[cur_elev][i]:
-                pass_num = pass_num + 1
-    return pass_num
+        for i in range(len(each_elev_stops[cur_elev])):
+            if src < each_elev_stops[cur_elev][i]:
+                stops_num += 1
+    return stops_num
 
 
-def PassNumDest(dest, cur_elev):
-    pass_num = 0
-    if status[cur_elev] != DOWN:
-        for i in range(len(num_of_stops[cur_elev])):
-            if dest > num_of_stops[cur_elev][i]:
-                pass_num = pass_num + 1
-    else:
-        for i in range(len(num_of_stops[cur_elev])):
-            if dest < num_of_stops[cur_elev][i]:
-                pass_num = pass_num + 1
-    return pass_num
+def PickUp(src: int, dest: int, allocated_elev: int):
+    pass
