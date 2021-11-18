48f2131305898b60bcda33566d039ffadaa53995
Orel Zamler <orelzx13@gmail.com>
2021-11-18 20:59:57 +0200

    syntax changes

--- Ex1/src/new_dir.py
+++ Ex1/src/new_dir.py
@@ -59,11 +59,11 @@ def calculateToSrc(self, cur_elev: int, src: int):
     stops_num = StopsOnWayToSrc(cur_elev, src)
     dif = abs(cur_location[cur_elev] - src)
     speed = self.elevators[cur_elev].get("_speed")
-    closeTime = self.elevators[cur_elev].get("_closeTime")
+    closeTime = self.elevators[cur_elev].get("_closeTime") * stops_num
     startTime = self.elevators[cur_elev].get("_startTime")
     stopTime = self.elevators[cur_elev].get("_stopTime")
     openTime = self.elevators[cur_elev].get("_openTime")
-    TimeToSrc = (dif / speed) + (closeTime + startTime + stopTime + openTime) * stops_num
+    TimeToSrc = (dif / speed) + closeTime + (startTime + stopTime + openTime) * (stops_num + 1)
     return TimeToSrc
 
 
@@ -84,11 +84,11 @@ def calculateFromSrc(self, dest: int, cur_elev: int, src: int):
     stops_num = StopsOnWayFromSrc(dest, cur_elev)
     dif = abs(src - dest)
     speed = self.elevators[cur_elev].get("_speed")
-    closeTime = self.elevators[cur_elev].get("_closeTime")
+    closeTime = self.elevators[cur_elev].get("_closeTime") * stops_num
     startTime = self.elevators[cur_elev].get("_startTime")
     stopTime = self.elevators[cur_elev].get("_stopTime")
     openTime = self.elevators[cur_elev].get("_openTime")
-    TimeToDest = (dif / speed) + (closeTime + startTime + stopTime + openTime) * stops_num
+    TimeToDest = (dif / speed) + closeTime + (startTime + stopTime + openTime) * (stops_num + 1)
     return TimeToDest
 
 
@@ -118,4 +118,4 @@ def PickUp(src: int, dest: int, allocated_elev: int):
 
 
 if __name__ == '__main__':
-    Offline("B4.json", "Calls_d.csv", "out_33.csv")
+    Offline("B1.json", "Calls_a.csv", "out_33.csv")
